ded4aa5bb3deaa9f38cf59eb839427876923b2a4
marien <mariencastellanosjr@gmail.com>
2023-07-05 22:15:32 -0400

    fixed deployed issues

--- Develop/script.js
+++ Develop/script.js
@@ -6,7 +6,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.textContent = password; // Update the value using textContent
+  passwordText.textContent = password; 
 
 }
 
@@ -18,37 +18,38 @@ var numbers = "1234567890";
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var special = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
-// Function to generate a random password based on the selected criteria
-function generatePassword() {
-  // Prompt for password length
-  var length = parseInt(prompt("Enter the length of the password (between 8 and 128):"));
 
+function generatePassword() {
+  // ask for length of PW
+  var length = parseInt(prompt("Enter the length of the password (between 8 and 128):"));
+  // validation for the length
   if (isNaN(length) || length < 8 || length > 128) {
     alert("Wrong password length.");
     return "";
   }
-
+  // pick which characters youd like
   var includeLowercase = confirm("Would you want to include lowercase characters? Cancel if not.");
   var includeUppercase = confirm("Would you want to include uppercase characters? Cancel if not.");
   var includeNumbers = confirm("Would you want to include numbers? Cancel if not.");
   var includeSpecial = confirm("Would you want to include special characters? Cancel if not.");
-
+  //character validation
   if (!includeLowercase && !includeUppercase && !includeNumbers && !includeSpecial) {
     alert("You must select a type of character. Please try again.");
     return "";
   }
-
+  //makes the promts work.
   var characters = "";
   if (includeLowercase) characters += lowercase;
   if (includeUppercase) characters += uppercase;
   if (includeNumbers) characters += numbers;
   if (includeSpecial) characters += special;
 
+  // math for the random aspect
   var password = "";
   for (let i = 0; i < length; i++) {
     var randomindex = Math.floor(Math.random() * characters.length);
     password += characters.charAt(randomindex);
   }
 
-  return password; // Return the generated password
+  return password; 
 }
